cb5633c2621828d7a8fbff4f44ee7917a8d8770d
palaksdarji <palaksdarji@gmail.com>
2019-11-30 14:08:15 +0530

    added comment as palak

--- app/src/main/java/com/palak/viewmodelkotlindemo/ui/main/MainFragment.kt
+++ app/src/main/java/com/palak/viewmodelkotlindemo/ui/main/MainFragment.kt
@@ -7,7 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.palak.viewmodelkotlindemo.R
-
+// palak
 class MainFragment : Fragment() {
 
     companion object {
